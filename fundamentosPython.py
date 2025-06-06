bb8528eed0b55bd15c4bc95b7c4b784afdf0089c
Zantyx-6 <santi2006cor@gmail.com>
2025-06-05 20:19:47 -0500

    Add files via upload

--- fundamentosPython.py
+++ fundamentosPython.py
@@ -72,17 +72,23 @@ else:
 
 #10. Máximo entre tres números
 #Dadas tres variables a, b y c, encuentra y muestra cuál es la mayor sin usar max().
-a = int(input("\nDigite un numero: "))
-b = int(input("Digite un numero: "))
-c = int(input("Digite un numero: "))
-if a>b or a>c:
-    print(f"El numero {a} es mayor que {b} y {c}")
-elif b>a or b>c:
-    print(f"El numero {b} es mayor que {a} y {c}")#---------------------------------------------------------------------------
-elif c>a or c>b:
-    print(f"El numero {c} es mayor que {a} y {b}")
+a = int(input("\nDigite un número: "))
+b = int(input("Digite otro número: "))
+c = int(input("Digite otro número: "))
+if a > b and a > c:
+    print(f"El número {a} es el mayor.")
+elif b > a and b > c:
+    print(f"El número {b} es el mayor.")
+elif c > a and c > b:
+    print(f"El número {c} es el mayor.")
+elif a == b and a > c:
+    print(f"{a} y {b} son los mayores, mayores que {c}.")
+elif a == c and a > b:
+    print(f"{a} y {c} son los mayores, mayores que {b}.")
+elif b == c and b > a:
+    print(f"{b} y {c} son los mayores, mayores que {a}.")
 else:
-    print("Los numeros son iguales")
+    print("Los tres números son iguales.")
 
 #Sección 2: Operadores Relacionales
 #11. Verificación de rango
@@ -180,17 +186,17 @@ else:
 #Dada una variable x, imprime si es un entero (int), flotante (float) o cadena (str).
 x = 1.0
 if type(x) == str:
-    print("El valor es string")
+    print("\nEl valor es string")
 elif type(x) == int:
-    print("El valor es int")
+    print("\nEl valor es int")
 elif type(x) == float:
-    print("El valor es float")
+    print("\nEl valor es float")
 
 #Sección 3: Operadores Lógicos (21–30)
 #21. ¿Puede votar?
 # Verifica si una persona puede votar. Debe tener al menos 18 años y tener documento de identidad (True o False).
-x = int(input("Digite su edad"))
-doc = input("Tienes documento? (s/n)")
+x = int(input("\nDigite su edad"))
+doc = input("Tienes documento? (s/n)").lower()
 if doc == "s":
     if x >= 18:
         print("Usted puede Votar")
@@ -199,20 +205,80 @@ if doc == "s":
 else:
     print("Usted no puede Votar sin documento")
 
+#22. Control de acceso lógico Una persona puede entrar si:
+#Tiene pase VIP, o Paga entrada y no está ebria. Evalúa esto con tres variables booleanas
+pase_vip = input("\n¿Tiene pase VIP? (s/n): ").lower() == 's'
+paga_entrada = input("¿Pago la entrada? (s/n): ").lower() == 's'
+ebria = input("¿Está ebria? (s/n): ").lower() == 's'
+if pase_vip or (paga_entrada and not ebria):
+    print("Puede entrar")
+else:
+    print("No puede entrar")
 
+#23. Validación XOR
+#Dadas dos condiciones booleanas cond1 y cond2, verifica si exactamente una es verdadera (operación XOR lógica).
+cond1 = input("\nDigite la primera condicion (True/False): ").lower() == 'true'
+cond2 = input("Digite la segunda condicion (True/False): ").lower() == 'true'
+if (cond1 and not cond2) or (not cond1 and cond2):
+    print("Una de las condiciones es verdadera")
+else:   
+    print("Ambas condiciones son verdaderas o ambas son falsas")
 
+#24. Validación compuesta múltiple
+#Dado un número n, verifica si es mayor que 0 y (múltiplo de 2 o de 3)
+n = int(input("\nDigite un numero para verificar si es mayor que 0 y multiplo de 2 o 3: "))
+if n > 0 and (n % 2 == 0 or n % 3 == 0):
+    print(f"El numero {n} es mayor que 0 y multiplo de 2 o 3")
+else:
+    print(f"El numero {n} NO es mayor que 0 o NO es multiplo de 2 o 3")
 
+#25. Condición contradictoria
+#Escribe una condición lógica que nunca se cumple (una contradicción), como x > 5 and x < 1.
+x = int(input("\nDigite un numero para verificar una contradiccion: "))
+if x > 5 and x < 1:
+    print(f"El numero {x} es mayor que 5 y menor que 1")
+else:
+    print(f"El numero {x} NO es mayor que 5 y menor que 1")
 
+#26. Negación lógica
+#Dada una condición x > 10, reescríbela usando not para que tenga el mismo efecto lógico.
+x = int(input("\nDigite un numero para verificar si es mayor que 10: "))
+if not (x <= 10):
+    print(f"El numero {x} es mayor que 10")
+else:
+    print(f"El numero {x} NO es mayor que 10")
 
+#27. Aprobación de estudiante
+#Verifica si un estudiante aprueba: nota >= 3.0 y asistencia >= 80.
+nota = float(input("\nDigite la nota del estudiante: "))
+asist= int(input("Digite el porcentaje de asistencia del estudiante: "))
+if nota >= 3.0 and asist >= 80:
+    print("El estudiante aprueba")
+else:
+    print("El estudiante NO aprueba :(")
 
+#28. Simulación de alarma
+#Si la temperatura es menor a 0 o mayor a 38 y la humedad es mayor a 80%, se activa una alarma.
+temp = float(input("\nDigite la temperatura actual: "))
+humedad = float(input("Digite el porcentaje de humedad: "))
+if (temp < 0 or temp > 38) and humedad > 80:
+    print("¡Alarma activada: Condiciones extremas detectadas !")
+else:
+    print("Condiciones normales, alarma desactivada.")
 
+#29. Contraseña segura Verifica si una contraseña cumple con:
+#Longitud mayor a 8 caracteres, Contiene al menos un número (Pista: puedes usar any(char.isdigit() for char in passw))
+password = input("\nDigite una contraseña segura: ")
+if len(password) > 8 and any(char.isdigit() for char in password):
+    print("Contraseña segura")
+else:
+    print("Contraseña insegura, debe tener más de 8 caracteres y al menos un número!!")
 
-
-
-
-
-
-
-
-
-
+#30. Doble negación lógica
+#Evalúa una expresión como: "No es falso que no tenga acceso" usando operadores lógicos. Traduce eso a Python.
+acceso = input("\n¿Tiene acceso? (s/n): ").lower() == 's'
+if not not acceso:
+    print("Tiene acceso")
+else:
+    print("No tiene acceso")
+ 
